3a090893852bceb36a3fdedcf274fe57ed5d6e73
Vlad-Ryzhov-GitHub <9globol6@gmail.com>
2024-08-18 09:55:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-# "1st program"
-a = 9
-b = a ** 0.5
-c = b * 5
-print(c)
-
-# "2nd program"
-a = 9.99
-b = 9.98
-c = 1000
-f = 1000.1
-r = (a > b and c != f)
-print (r)
-
-# "3rd program"
-a = 2*2+2
-b = 2*(2+2)
-print (a == b)
-
-# "4th program"
-a = float('123.456')
-b = a * 10
-c = int(b % 10)
-print(c)
-
+example = 'Топинамбур'
+print (example[0])
+print (example[-1])
+print (example[5:10])
+print (example[::-1])
+print (example[1:10:2])
