6056bd6859eb5dab5421b86948907326b1176531
Vlad-Ryzhov-GitHub <9globol6@gmail.com>
2024-08-31 14:07:48 +0300

    Delete module_1_5.py

--- module_1_5.py
+++
@@ -1,9 +0,0 @@
-immutable_var = ( 1, 2, 'a', 'b')
-print(immutable_var)
-mutable_list = [ 1, 2, 'a', 'b' ]
-mutable_list.append('Modified')
-print(mutable_list)
-immutable_var[1] = 100 # Приводит к ошибке так как кортежи не изменяемые
-print(immutable_var)
-
-
