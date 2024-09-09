88f99a264bf3fbeafa14873dc20942e6b9b81e41
Vlad-Ryzhov-GitHub <9globol6@gmail.com>
2024-09-09 14:59:49 +0300

    Delete module_2_2..py

--- module_2_2..py
+++
@@ -1,9 +0,0 @@
-first = int(input('Ввести первое число: '))
-second = int(input('Ввести второе число: '))
-third = int(input('Ввести третие число: '))
-if first == second == third:
-    print(3)
-elif first == second or first != third or second != third:
-    print(2)
-else:
-    print(0)
